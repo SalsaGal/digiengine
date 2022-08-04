35713918382cfec122d472d45241704889350ba5
SalsaGal <fielddashos@gmail.com>
2022-08-04 11:49:15 +1000

    Render background image

--- src/main.rs
+++ src/main.rs
@@ -6,7 +6,7 @@ use std::fs::read_to_string;
 use crate::system::System;
 
 use clap::Parser;
-use current::*;
+use current::{*, sprite::{Sprite, Transform}};
 use save::Save;
 
 #[derive(Parser)]
@@ -24,6 +24,8 @@ fn main() {
 struct Digiengine {
     save: Option<Save>,
     system: System,
+
+    background: Sprite,
 }
 
 impl Game for Digiengine {
@@ -35,7 +37,7 @@ impl Game for Digiengine {
 
         std::env::set_current_dir(args.system_path).unwrap();
 
-        let system = serde_json::from_str(&read_to_string("system.json").unwrap()).unwrap();
+        let system: System = serde_json::from_str(&read_to_string("system.json").unwrap()).unwrap();
         let save_path = args.save_path.unwrap_or_else(|| "save.json".to_owned());
         let save = if let Ok(contents) = read_to_string(save_path) {
             Some(serde_json::from_str(&contents).unwrap())
@@ -43,9 +45,21 @@ impl Game for Digiengine {
             None
         };
 
+        let background = Sprite::new_path_rect(data.graphics, &system.background, sprite::Filter::Nearest)
+            .with_transform(Transform::scale((2.0, 2.0).into()).with_translation((0.0, 0.0, 0.0).into()));
+
         dbg!(&system);
         dbg!(&save);
 
-        Self { save, system }
+        Self {
+            save,
+            system,
+
+            background,
+        }
+    }
+
+    fn render<'a>(&'a mut self, mut frame: graphics::Frame<'a>) {
+        self.background.render_to(&mut frame);
     }
 }
