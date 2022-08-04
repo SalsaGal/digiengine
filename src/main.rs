19ea9d6a5128608d281226aa36ef177cf6937030
SalsaGal <fielddashos@gmail.com>
2022-08-04 10:38:08 +1000

    Support working directories

--- src/main.rs
+++ src/main.rs
@@ -7,7 +7,11 @@ use current::*;
 
 #[derive(Parser)]
 struct Args {
+    /// The configuration file to read system information from
     config: String,
+    /// The working directory to find system folders
+    #[clap(long = "pwd")]
+    working_dir: Option<String>,
 }
 
 fn main() {
@@ -24,6 +28,14 @@ impl Game for Digiengine {
         data.graphics.background_color = wgpu::Color::WHITE;
 
         let args = Args::parse();
+
+        if let Some(dir) = args.working_dir {
+            std::env::set_current_dir(dir).expect("Invalid working directory: {dir}");
+        } else if !cfg!(debug_assertions) {
+            let dir = std::env::current_exe().unwrap();
+            std::env::set_current_dir(dir).unwrap();
+        }
+
         let system_config = std::fs::read_to_string(&args.config).unwrap();
         let system = serde_json::from_str(&system_config).unwrap();
 
